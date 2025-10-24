d1fbd616e72bc8e623529a51de08de9ac116ba4f
williammelomel7-debug <williammelomel7@gmail.com>
2025-10-23 23:55:17 -0300

    Add files via upload

---
+++ oto1-pp/js/eyYfbG3731695.js
@@ -0,0 +1,121 @@
+
+      function atomiShowItems({items}) {
+      try {
+        items.forEach((item) => {
+          const hiddenItem = [...document.querySelectorAll(`#${item}`), ...document.querySelectorAll(`.${item}`)];
+          console.log("hiddenItem", hiddenItem)
+          if (hiddenItem?.length > 0) {
+            hiddenItem.forEach(item => item.classList.remove("atomicat-delay"));
+          }
+        })
+      } catch (error) {
+        console.log(error);
+      }
+      }
+    
+      function runDelayedFunctions(data) {
+        try {
+          document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+          if(data?.setDisplayed){
+            localStorage.setItem(data?.setDisplayed, true);
+          }
+          
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+    (function() {
+      try {
+        document.addEventListener('DOMContentLoaded', function () {
+          document.addEventListener("keydown", function (e) {
+            e.ctrlKey && e.preventDefault();
+          }),
+          (document.onkeydown = function (e) {
+            if (123 == e.keyCode) return !1;
+          }),
+          document.addEventListener("contextmenu", (e) => e.preventDefault());
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    (function() {
+          try {
+              const clickeventList = [{"compKey":"86f156e","misc":{"type":"button"}},{"compKey":"f5efa3a","misc":{"type":"button"}},{"compKey":"a6f5bb8","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
+          (function() {
+            try {
+              var vturbvideoId = "68facef49a717d043c8e5d65";
+              var compKey = "1cbee73";
+              const twr = false;
+              var SECONDS_TO_DISPLAY = 800;
+              var attempts = 0;
+              var elsDisplayed = false;
+              var alreadyDisplayedKey = 'alreadyElsDisplayed800';
+              var alreadyElsDisplayed = localStorage.getItem(alreadyDisplayedKey);
+
+              var showHiddenElements = function () {
+                elsDisplayed = true;
+                runDelayedFunctions();
+                localStorage.setItem(alreadyDisplayedKey, true);
+              };
+              function getVideoInstance() {
+                if (smartplayer.instances.length > 1) {
+                  return smartplayer.instances.find(
+                    (instance) => (instance?.options?.id || instance?.analytics?.player?.options?.id) === vturbvideoId
+                  );
+                }
+                return smartplayer.instances[0];
+              };
+              window.startWatchVideoProgress = function startWatchVideoProgress(reAttempts) {
+                if (reAttempts) {
+                  attempts = reAttempts;
+                }
+                
+                console.log("vturbvideoId", vturbvideoId);
+                if (typeof smartplayer === 'undefined' || !(smartplayer.instances && smartplayer.instances.length)) {
+                  if (attempts >= 10) return;
+                  attempts += 1;
+                  return setTimeout(function () {
+                    startWatchVideoProgress();
+                  }, 1000);
+                }
+                console.log(smartplayer.instances);
+                var videoInstance = getVideoInstance();
+                videoInstance.on('timeupdate', () => {
+                  if (elsDisplayed || videoInstance.smartAutoPlay) return;
+                  console.log("currentTime => " +videoInstance.video.currentTime +" SECONDS_TO_DISPLAY => " +SECONDS_TO_DISPLAY);
+                  if (videoInstance.video.currentTime < SECONDS_TO_DISPLAY) return;
+                  showHiddenElements();
+                });
+              };
+              if (alreadyElsDisplayed === 'true') {
+                setTimeout(function () {
+                  showHiddenElements();
+                }, 100);
+              } else {
+                window.startWatchVideoProgress();
+              }
+            } catch (error) {
+              console.log(error);
+            }
+            
+          })();
+        
